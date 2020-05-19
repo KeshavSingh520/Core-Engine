ad1abe4e64a859c7b392d0de422ed4f6d0a8a29e
Keshav <you@example.com>
2020-05-19 13:14:37 +0530

    fix

--- src/test/java/utils/BrowserHandler.java
+++ src/test/java/utils/BrowserHandler.java
@@ -5,7 +5,7 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -14,11 +14,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.Getter;
 
 @Getter
-public class BrowserHandler{
+public class BrowserHandler {
 
-	private  String strBrowser;
+	private String strBrowser;
 	private String strUrl;
-	private static ThreadLocal<WebDriver> threadDriver= new ThreadLocal<>();
+	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
 	private int implicitWait = 10;
 	ChromeOptions cap = new ChromeOptions();
 
@@ -26,42 +26,47 @@ public class BrowserHandler{
 		CHROME, FIREFOX, EDGE, SAFARI
 	}
 
-	public BrowserHandler(String browser,String url,String strhub) throws MalformedURLException {
-		this.strUrl=url;
-		this.strBrowser=browser;
+	public BrowserHandler(String browser, String url, String strhub, String strGridEnabled) throws MalformedURLException {
+		this.strUrl = url;
+		this.strBrowser = browser;
 		switch (browsersList.valueOf(browsersList.class, strBrowser.toUpperCase())) {
 		case CHROME:
-			threadDriver.set(createChromeDriver(strhub));
+			threadDriver.set(createChromeDriver(strhub,strGridEnabled));
 		case FIREFOX:
 		case EDGE:
 		case SAFARI:
 		}
 	}
 
-	public WebDriver createChromeDriver(String strhub) throws MalformedURLException {
-		WebDriver driver=null;
+	public WebDriver createChromeDriver(String strhub, String strGridEnabled) throws MalformedURLException {
+		WebDriver driver = null;
 		if (driver == null) {
-			System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-			WebDriverManager.chromedriver().setup();
-			driver = new RemoteWebDriver(new URL(strhub),cap);
+			System.setProperty("webdriver.chrome.silentOutput", "true");
+			//WebDriverManager.chromedriver().setup();
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "/chromedriver.exe");
+			if (strGridEnabled.equalsIgnoreCase("Y")) {
+				driver = new RemoteWebDriver(new URL(strhub), cap);
+			} else {
+				driver = new ChromeDriver(cap);
+			}
 			driver.manage().timeouts().implicitlyWait(implicitWait, TimeUnit.SECONDS);
 			driver.get(strUrl);
 		}
 		return driver;
 	}
-	
+
 	@Test()
-	public void get(){
+	public void get() {
 		System.out.println(strUrl);
 	}
-	
+
 	public void launchApplicationUrl(WebDriver driver) {
 		driver.get(strUrl);
 	}
-	
-	public static WebDriver getWebdriver(){
+
+	public static WebDriver getWebdriver() {
 		return threadDriver.get();
 	}
-	
 
 }
