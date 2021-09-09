e91f135f62b08f8b452ce6e3d89f1be870d86be2
Keshav <keshav@hp.com>
2021-09-09 17:16:29 +0530

    vgdfg

--- src/test/java/utils/BrowserHandler.java
+++ src/test/java/utils/BrowserHandler.java
@@ -3,13 +3,25 @@ package utils;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.session.CapabilitiesFilter;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
+import io.appium.java_client.service.local.flags.ServerArgument;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.Getter;
 
@@ -21,34 +33,63 @@ public class BrowserHandler {
 	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
 	private int implicitWait = 10;
 	ChromeOptions cap = new ChromeOptions();
+	public AppiumServiceBuilder builder;
+	public AppiumDriverLocalService service;
+	
 
 	private enum browsersList {
 		CHROME, FIREFOX, EDGE, SAFARI
 	}
 
-	public BrowserHandler(String browser, String url, String strhub, String strGridEnabled) throws MalformedURLException {
+	public BrowserHandler(String browser, String url, String strhub, String strGridEnabled,String mobileBrowser) throws MalformedURLException {
 		this.strUrl = url;
 		this.strBrowser = browser;
 		switch (browsersList.valueOf(browsersList.class, strBrowser.toUpperCase())) {
 		case CHROME:
-			threadDriver.set(createChromeDriver(strhub,strGridEnabled));
+			threadDriver.set(createChromeDriver(strhub,strGridEnabled,mobileBrowser));
 		case FIREFOX:
 		case EDGE:
 		case SAFARI:
 		}
 	}
 
-	public WebDriver createChromeDriver(String strhub, String strGridEnabled) throws MalformedURLException {
+	public WebDriver createChromeDriver(String strhub, String strGridEnabled,String mobileBrowser) throws MalformedURLException {
 		WebDriver driver = null;
 		if (driver == null) {
 			System.setProperty("webdriver.chrome.silentOutput", "true");
-			//WebDriverManager.chromedriver().setup();
-			System.setProperty("webdriver.chrome.driver",
-					System.getProperty("user.dir") + "/chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
+//			System.setProperty("webdriver.chrome.driver",
+//					System.getProperty("user.dir") + "/chromedriver.exe");
+			if (strGridEnabled.equalsIgnoreCase("Y")) {
+				driver = new RemoteWebDriver(new URL(strhub), cap);
+			} else if(mobileBrowser.equalsIgnoreCase("Y")){
+				AppiumDriverLocalService service = startAppium();
+				cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+				cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0.6");
+				cap.setCapability(MobileCapabilityType.UDID, "cdaed149");
+				cap.setCapability(MobileCapabilityType.DEVICE_NAME, "OnePlus3T");
+				cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+				cap.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+				cap.setCapability("chromedriverExecutable", "D:\\Software\\Chrome93\\chromedriver.exe");
+				driver = new RemoteWebDriver(new URL(service.getUrl().toString()),cap);
+			}else {
+				driver = new ChromeDriver();
+			}
+			driver.manage().timeouts().implicitlyWait(implicitWait, TimeUnit.SECONDS);
+			driver.get(strUrl);
+		}
+		return driver;
+	}
+	
+	public WebDriver createFirefoxDriver(String strhub, String strGridEnabled) throws MalformedURLException {
+		WebDriver driver = null;
+		if (driver == null) {
+			System.setProperty("webdriver.chrome.silentOutput", "true");
+			WebDriverManager.firefoxdriver().setup();
 			if (strGridEnabled.equalsIgnoreCase("Y")) {
 				driver = new RemoteWebDriver(new URL(strhub), cap);
 			} else {
-				driver = new ChromeDriver(cap);
+				driver = new FirefoxDriver(cap);
 			}
 			driver.manage().timeouts().implicitlyWait(implicitWait, TimeUnit.SECONDS);
 			driver.get(strUrl);
@@ -67,6 +108,21 @@ public class BrowserHandler {
 
 	public static WebDriver getWebdriver() {
 		return threadDriver.get();
+	}
+	
+	public AppiumDriverLocalService startAppium() {
+		if(service==null) {
+		builder = new AppiumServiceBuilder();
+		builder.withIPAddress("127.0.0.1");
+		builder.usingAnyFreePort();
+		builder.withArgument((ServerArgument)GeneralServerFlag.LOG_LEVEL,"info");
+		service = AppiumDriverLocalService.buildService(builder);
+		System.out.println("Starting Appium Service:  " + service.getUrl());
+		service.start();
+		}else {
+			System.out.println("Appium Service already running:  " + service.getUrl());
+		}
+		return service;
 	}
 
 }
